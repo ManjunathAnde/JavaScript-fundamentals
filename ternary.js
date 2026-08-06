ce560237e1ac9f6bdd328cc811c4ac259c40298c
ManjunathAnde <manjunathande7@gmail.com>
2026-08-06 18:06:52 +0530

    Template literals

--- ternary.js
+++ ternary.js
@@ -1,4 +1,12 @@
+//TERNARY OPERATORS
 let age =12
 const status= age>=18? "adult" : "not adult"
 //a conditional variable which takes "adult" ig age>=18 or takes "not adult"\
 console.log(status)
+
+
+//TEMPLATE LITERAL
+let name = "Gilfoyle"
+let company = "Pied Piper"
+const greeting = `Hello ${name}, you are from ${company}`; //Backticks
+console.log(greeting)
