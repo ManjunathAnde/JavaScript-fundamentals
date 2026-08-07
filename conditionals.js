e39f21817bdedbbf13955f8bd6cea7706390766b
ManjunathAnde <manjunathande7@gmail.com>
2026-08-07 11:38:35 +0530

    Conditionals in JS

---
+++ conditionals.js
@@ -0,0 +1,13 @@
+//Conditionals
+let age =35
+
+if (age>=60) {
+    console.log("Senior citizen")
+}
+else if (age>40 && age<=59) {
+    console.log("Middle age")
+}
+else {
+    console.log("Young age")
+}
+
--- ternary.js
+++ ternary.js
@@ -1,7 +1,7 @@
 //TERNARY OPERATORS
 let age =12
 const status= age>=18? "adult" : "not adult"
-//a conditional variable which takes "adult" ig age>=18 or takes "not adult"\
+//a conditional variable which takes "adult" if age>=18 or takes "not adult"\
 console.log(status)
 
 
