20e7f5200e0fec9a2a1b322045e9591f5a22e866
ManjunathAnde <manjunathande7@gmail.com>
2026-08-07 11:43:06 +0530

    For loops in js

---
+++ loops.js
@@ -0,0 +1,8 @@
+
+// for: use when we know how many times / iterating a known range
+// start value of j =0 ; post increment j until j remains less than 5
+for (let j = 0; j < 5; j++) {
+  console.log(j);
+}
+
+
