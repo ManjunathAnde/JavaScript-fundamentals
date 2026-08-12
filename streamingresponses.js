5287ececf103f9a4ed881db0dbfe8ba348900f73
ManjunathAnde <manjunathande7@gmail.com>
2026-08-12 22:24:30 +0530

    Streaming responses exampple and for await

--- streamingresponses.js
+++ streamingresponses.js
@@ -1,4 +1,10 @@
 // This waits for the message to arrive and then prints entire message at once
 const completion = await groq.chat.completions.create({message});
-console.log(completion.choices[0].message.content); 
+console.log(completion.choices[0].message.content);  //Non streaming responses
+
+// Streaming: get the response in small pieces (chunks) as it's generated
+const stream = await groq.chat.completions.create({ message, stream: true });
+for await (const chunk of stream) {  // printing the content using for await loop
+  process.stdout.write(chunk.choices[0].delta.content || "");
+}
 
